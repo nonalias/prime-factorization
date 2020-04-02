0e9c8586695234f8566bc16b4d50d31cb9dac737
taehkim <taehkim@student.42seoul.kr>
2020-04-03 00:14:30 +0900

    주석처리

--- factorization.c
+++ factorization.c
@@ -6,13 +6,14 @@
 /*   By: taehkim <taehkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/01 20:05:43 by taehkim           #+#    #+#             */
-/*   Updated: 2020/04/01 22:10:01 by taehkim          ###   ########.fr       */
+/*   Updated: 2020/04/03 00:14:22 by taehkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <limits.h>
 
+// str이 숫자로만 이루어져있는지 확인, 반환값은 str의 총 길이
 int	validation(char *str)
 {
 	int		count;
@@ -27,16 +28,22 @@ int	validation(char *str)
 	return (count);
 }
 
+// string 을 integer로 바꾸어주는 함수
+// 잘못된 숫자형식일 경우 -1반환, 범위오류일경우 0반환
+// 정상 변환되면 1 반환
 int	stoi(char *str, unsigned int *number)
 {
 	int		size;
 	long long 	helper;
 	long long	result;
 
+	// 숫자로만 이루어지지 않을 때
 	if ((size = validation(str)) == -1)
 		return (-1);
+	// 10자리 수가 넘어가면
 	if (size > 10)
 		return (0);
+	// string을 integer로 바꾸어주는 작업
 	result = 0;
 	helper = 1;
 	while (--size >= 0)
@@ -44,12 +51,14 @@ int	stoi(char *str, unsigned int *number)
 		result += (helper * (str[size] - '0'));
 		helper = helper * 10;
 	}
+	// 지시된 범위를 벗어났을 때
 	if (result > UINT_MAX || result < 2)
 		return (0);
 	*number = result;
 	return (1);
 }
 
+// 제곱근의 근사 값을 구해주는 함수
 int	get_near_sqrt(unsigned int number)
 {
 	unsigned int	output;
@@ -60,6 +69,7 @@ int	get_near_sqrt(unsigned int number)
 	return ((int)(output - 1));
 }
 
+// 소수 판별 함수, 1이면 소수, 0이면 소수가 아님
 int	is_prime(unsigned int number)
 {
 	int		i;
@@ -80,11 +90,13 @@ int	main(int argc, char *argv[])
 	unsigned int	i;
 	int		temp;
 
+	// 입력 인자 갯수 오류 출력
 	if (argc != 2)
 	{
 		printf("The count of inputs is incorrect\n");
 		return (1);
 	}
+	// 오류 코드에 따라 메시지 출력
 	if ((temp = stoi(argv[1], &number)) <= 0)
 	{
 		if (temp == 0)
@@ -93,6 +105,7 @@ int	main(int argc, char *argv[])
 			printf("Your input number is WRONG\n");
 		return (1);
 	}
+	// 약수이면서 소수이면 출력
 	i = 2;
 	while (i <= number)
 	{
